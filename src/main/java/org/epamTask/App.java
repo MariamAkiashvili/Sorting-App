675c79cf379511f41ef4207339603e0a51a72844
Mariam Akiashvili <mari.akiashvili@gmail.com>
2024-04-25 22:08:44 +0400

    Initial Commit

---
+++ src/main/java/org/epamTask/App.java
@@ -0,0 +1,26 @@
+package org.epamTask;
+
+import java.util.Arrays;
+
+public class App 
+{
+    public int []  sort(int[] args)
+    {
+//        int[] arr = new int[10];
+
+        if(args == null){
+            throw new IllegalArgumentException("Too many arguments");
+        }
+        if(args.length > 10){
+            throw new IllegalArgumentException("Too many arguments");
+        }
+
+        Arrays.sort(args);
+
+        for (int arg : args) {
+            System.out.println(arg);
+        }
+
+        return args;
+    }
+}
